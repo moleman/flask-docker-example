4797ebde77183348e6b735c6166bedefd5b0ac6a
Pierre Dahlström <pierre.dahlstrom@vaimo.com>
2017-04-19 09:34:22 +0200

    Add MySQL

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM ubuntu:16.04
 MAINTAINER Pierre <pierre.dahlstrom@vaimo.com>
 
 RUN apt-get update -y && \
-    apt-get install -y python-pip python-dev
+    apt-get install -y python-pip python-dev libmysqlclient-dev
 
 COPY ./requirements.txt /app/requirements.txt
 
--- app.py
+++ app.py
@@ -2,12 +2,28 @@ import os
 
 from flask import Flask
 from redis import Redis
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
+# MySQL configurations
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'database'
+app.config['MYSQL_HOST'] = 'mysql'
+
+mysql = MySQL(app)
+
 @app.route('/')
 def hello_world():
     redis.incr('hits')
-    return 'Hey, we have Flask in a Docker container! Host: %s. TEST 6!!!!! Hits: %s' % \
+    return 'Hey, we have Flask in a Docker container! Host: %s. TEST 7!!!!! Hits: %s' % \
         (os.environ.get('HOSTNAME'), redis.get('hits'))
+
+@app.route('/versions')
+def users():
+    cur = mysql.connection.cursor()
+    cur.execute('SHOW VARIABLES LIKE "%version%";')
+    rv = cur.fetchall()
+    return str(rv)
